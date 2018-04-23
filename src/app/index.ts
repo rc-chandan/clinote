bb9a239f72a43c6f37e84bb7a22ce3339af16501
rc-chandan <chandan316@outlook.com>
2018-04-23 18:27:42 +0530

    added husky precommit hook

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,5 @@
 import * as os from 'os';
 
-import * as databaseUtil from '../database/index';
 import eventTypes from '../events/eventTypes';
 import events from '../events';
 import { checkOSSupport, getConfiguration, IConfig } from '../utils/index';
@@ -13,9 +12,7 @@ export async function initiateSession() {
 }
 
 function startApp({ dbFilePath }: IConfig) {
-  databaseUtil.connect(dbFilePath);
   events.emit(eventTypes.GET_ALL_NOTES);
-  // cliUtil.attachEventListners(eventListners)
 }
 
 function encrypt(passphrase: string) {
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,23 +1,23 @@
-export function getAllNotesForUser(uid: number): INote[] {
-  return [];
-}
+// export function getAllNotesForUser(uid: number): INote[] {
+//   return [];
+// }
 
-export function getNoteById(noteId: string): INote {
-  return {};
-}
+// export function getNoteById(noteId: string): INote {
+//   return {};
+// }
 
-export function createNote(note: INote): INote {
-  return {};
-}
+// export function createNote(note: INote): INote {
+//   return {};
+// }
 
-export function updateNote(note: INote): INote {
-  return {};
-}
+// export function updateNote(note: INote): INote {
+//   return {};
+// }
 
-export function deleteNote(note: INote): INote {
-  return {};
-}
+// export function deleteNote(note: INote): INote {
+//   return {};
+// }
 
-export function connect(dbFilePath: string): Connection {
-  return {};
-}
+// export function connect(dbFilePath: string): Connection {
+//   return {};
+// }
