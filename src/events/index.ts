2772dd86caa3d2d77ed2d95782e7685f6984c400
rc-chandan <chandan316@outlook.com>
2018-06-06 13:22:53 +0530

    compilation issues suppressed

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,15 +1,15 @@
 import * as EventEmitter from 'events';
 import eventTypes from './eventTypes';
-import { INote } from '../types';
+// import { INote } from '../types';
 
 const emitter = new EventEmitter();
 
-emitter.on(eventTypes.ADD_NOTE, (note: INote) => {
-  console.info(note);
-});
+// emitter.on(eventTypes.ADD_NOTE, (note: INote) => {
+//   console.info(note);
+// });
 
-emitter.on(eventTypes.GET_ALL_NOTES, (notes: INote[]) => {
-  console.info('get all notes called');
-});
+// emitter.on(eventTypes.GET_ALL_NOTES, (notes: INote[]) => {
+//   console.info('get all notes called');
+// });
 
 export default emitter;
